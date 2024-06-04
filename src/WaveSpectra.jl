f45ebde694b56684668687bc189fdc39ea7403dd
Carlos A. Michelén Ströfer <c.michelen.strofer@gmail.com>
2024-06-04 17:52:07 -0600

    parametric type

--- src/WaveSpectra.jl
+++ src/WaveSpectra.jl
@@ -1,70 +1,122 @@
 module WaveSpectra
 
-export WaveSpectrum, isdiscrete, isunitful
+export WaveSpectrum, isdiscrete, isunitful, quantity
+# export integrate, ∫, spectral_moment, energy_period, significant_waveheight
 
 using Interpolations: linear_interpolation
 using Plots: @recipe
-using Unitful: Frequency, unit, Hz
+using Unitful: Frequency, Hz, upreferred
+import Unitful: unit, dimension, Quantity
+using Integrals: IntegralProblem, solve, QuadGKJL
+using SciMLBase: AbstractIntegralAlgorithm, ReturnCode
 import Base: Exception, showerror
 
+
 # Struct definition
-struct WaveSpectrum
+struct WaveSpectrum{D, U}
     func::Union{Nothing, Function}
     frequency::Union{Nothing, AbstractVector{<:Number}}
-    density::Union{Nothing, AbstractVector{<:Number}}
+    value::Union{Nothing, AbstractVector{<:Number}}
 
-    function WaveSpectrum(func, frequency, density)
-        if !(isnothing(frequency) && isnothing(density)) && (size(frequency) !== size(density))
-            error("Frequencies and Value Arrays are not same size!")
+    function WaveSpectrum{D,U}(func, frequency, value) where {D,U}
+        # check parameters
+        if !(isa(D, Bool) && isa(U, Bool))
+            error("parameters must be booleans")
         end
-        return new(func, frequency, density)
+        # check arguments
+        option_1 = !isnothing(func) && isnothing(frequency) && isnothing(value) # T F F
+        option_2 = !isnothing(func) && !isnothing(frequency) && isnothing(value) # T T F
+        option_3 = isnothing(func) && !isnothing(frequency) && !isnothing(value) # F T T
+        function check_frequency(frequency)
+            (eltype(frequency)<:Frequency) || error("unknown units for `frequency` vector")
+        end
+        if !(option_1 || option_2 || option_3)
+            error("bad combination of input arguments")
+        end
+        if option_3 && (size(frequency) !== size(value))
+            error("`frequency` and `value` arrays are not same size!")
+        end
+        (option_2||option_3) && U && check_frequency(frequency)
+        # create parametric object
+        option_2 && (value = func.(frequency))
+        return new{D,U}(func, frequency, value)
     end
 end
 
 # Constructors
-function WaveSpectrum(freqs::AbstractVector{<:Number}, vals::AbstractVector{<:Number}; interpolation::Function=linear_interpolation)
-    extrapolation = interpolation(freqs, vals; extrapolation_bc = 0)
-    return WaveSpectrum(x->extrapolation(x), freqs, vals)
+function WaveSpectrum(func::Function; density=true, units=true)
+    # option 1
+    return WaveSpectrum{density}{units}(func, nothing, nothing)
 end
 
-WaveSpectrum(func::Function) = WaveSpectrum(func, nothing, nothing)
-WaveSpectrum(func::Function, freqs::AbstractVector{<:Number}) = WaveSpectrum(func, freqs, func(freqs))
-WaveSpectrum(S::WaveSpectrum, freqs::AbstractVector{<:Number}) = WaveSpectrum(S.func, freqs)
+function WaveSpectrum(func::Function, freqs::AbstractVector{<:Number}; density=true)
+    # option 2
+    units = (eltype(freqs) <: Quantity) ? true : false
+    return WaveSpectrum{density}{units}(func, freqs)
+end
+
+function WaveSpectrum(S::WaveSpectrum, freqs::AbstractVector{<:Number})
+    # option 2b
+    density, units = typeof(S).parameters
+    return WaveSpectrum(S.func, freqs; density, units)
+end
+
+function WaveSpectrum(freqs::AbstractVector{<:Number}, vals::AbstractVector{<:Number}; density=true, interpolation::Function=linear_interpolation)
+    # option 3
+    units = (eltype(vals) <: Quantity) ? true : false
+    func = x->interpolation(freqs, vals; extrapolation_bc = 0)(x)
+    S = WaveSpectrum{density, units}(func, freqs, nothing)
+    @assert (S.value  ≈ vals)
+    return S
+end
 
 # Interface
-isdiscrete(S::WaveSpectrum) = !isnothing(S.density) && !isnothing(S.frequency)
-isunitful(S::WaveSpectrum) = hasmethod(S.func, (Frequency,)) && !(hasmethod(S.func, (Any,)))
+isdiscrete(S::WaveSpectrum) = !isnothing(S.value) && !isnothing(S.frequency)
+isdensity(S::WaveSpectrum) = typeof(S) <: WaveSpectrum{true, U} where U
+isunitful(S::WaveSpectrum) = typeof(S) <: WaveSpectrum{D, true} where D
+unit(S::WaveSpectrum) = unit(eltype(S.value))
+dimension(S::WaveSpectrum) = dimension(eltype(S.value))
+function quantity(S::WaveSpectrum{true,true})
+    dims = dimension(S) * dimension(eltype(S.frequency))
+    units = upreferred(unit(S) * unit(eltype(S.frequency)))
+    return dims, units
+end
 
 (S::WaveSpectrum)(freq::Number) =  S.func(freq)
 
 mutable struct DiscreteError <: Exception
     message::String
-    DiscreteError() = new("WaveSpectrum instance was treated as discrete when it is continuous!")
+    DiscreteError() = new("continuous `WaveSpectrum` instance was treated as discrete")
 end
 Base.showerror(io::IO, e::DiscreteError) = print(io, "DiscreteError: $(e.message)")
 
 _firststate() = 1
-Base.getindex(S::WaveSpectrum, i::Int) = isdiscrete(S) ? S.density[i] : throw(DiscreteError())
+Base.getindex(S::WaveSpectrum, i::Int) = isdiscrete(S) ? S.value[i] : throw(DiscreteError())
 Base.firstindex(S::WaveSpectrum) = isdiscrete(S) ? _firststate() : throw(DiscreteError())
-Base.lastindex(S::WaveSpectrum) = isdiscrete(S) ? length(S.density) : throw(DiscreteError())
-Base.length(S::WaveSpectrum) = isdiscrete(S) ? length(S.density) : throw(DiscreteError())
-Base.size(S::WaveSpectrum) = isdiscrete(S) ? size(S.density) : throw(DiscreteError())
-Base.eltype(S::WaveSpectrum) = isdiscrete(S) ? eltype(S.density) : throw(DiscreteError())
-Base.iterate(S::WaveSpectrum) = isdiscrete(S) ? (S.density[firstindex(S)], _firststate()+1) : throw(DiscreteError())
+Base.lastindex(S::WaveSpectrum) = isdiscrete(S) ? length(S.value) : throw(DiscreteError())
+Base.length(S::WaveSpectrum) = isdiscrete(S) ? length(S.value) : throw(DiscreteError())
+Base.size(S::WaveSpectrum) = isdiscrete(S) ? size(S.value) : throw(DiscreteError())
+Base.eltype(S::WaveSpectrum) = isdiscrete(S) ? eltype(S.value) : throw(DiscreteError())
+Base.iterate(S::WaveSpectrum) = isdiscrete(S) ? (S.value[firstindex(S)], _firststate()+1) : throw(DiscreteError())
 function Base.iterate(S::WaveSpectrum, state::Int)
     isdiscrete(S) || throw(DiscreteError())
-    return (state<=length(S)) ? (S.density[state], state+1) : nothing
+    return (state<=length(S)) ? (S.value[state], state+1) : nothing
 end
 
 # Plots recipes
-_labels() = ("Frequency", "Spectral density")
+function _labels(S::WaveSpectrum)
+    x_label = "frequency"
+    y_label = isdensity(S) ? "spectral density" : "discrete (integral) spectrum"
+    return (x_label, y_label)
+end
+
 @recipe function f(S::WaveSpectrum, args...)
-    _xlabel, _ylabel = _labels()
+    _xlabel, _ylabel = _labels(S)
     xlabel --> _xlabel
     ylabel --> _ylabel
     if isdiscrete(S)
         marker := :auto
-        (S.frequency, S.density, args...)
+        (S.frequency, S.value, args...)
     else
         if isempty(args)
             isunitful(S) ? (args=(0Hz, 10Hz)) : (args=(0, 10))
@@ -74,12 +126,13 @@ _labels() = ("Frequency", "Spectral density")
 end
 
 @recipe function f(S::WaveSpectrum, freq::AbstractVector{<:Number}, args...)
-    _xlabel, _ylabel = _labels()
+    _xlabel, _ylabel = _labels(S)
     xlabel --> _xlabel
     ylabel --> _ylabel
     marker := :auto
     (freq, S.(freq), args...)
 end
 
+# include("statistics.jl")
 
 end #Module end
--- src/statistics.jl
+++ src/statistics.jl
@@ -1,86 +1,32 @@
-# spectral statistics
-function integrate(y::AbstractVector, x::AbstractVector, method::AbstractIntegralAlgorithm=TrapezoidalRule())
-    sol = solve(SampledIntegralProblem(y, x), method)
+
+
+function integrate(func::Function, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing; alg::AbstractIntegralAlgorithm=QuadGKJL())
+    isnothing(f_begin) && (f_begin = isunitful(func) ? 0Hz : 0)
+    isnothing(f_end) && (f_end = isunitful(func) ? Inf*Hz : Inf)
+    sol = solve(IntegralProblem((x,p)->func(x), (f_begin, f_end), nothing), alg)
     sol.retcode ≠ ReturnCode.Success && error("solution unsuccessful with code: $(sol.retcode)")
     return sol.u
 end
 
-function integrate(y::AbstractMatrix, x::AbstractVector, method::AbstractIntegralAlgorithm=TrapezoidalRule())
-    sol = solve(SampledIntegralProblem(y, x; dim=2), method)
-    sol.retcode ≠ ReturnCode.Success && error("solution unsuccessful with code: $(sol.retcode)")
-    return sol.u
-end
-
-function integrate(y::AbstractVector, x::AbstractVector, a::Number, b::Number, method::AbstractIntegralAlgorithm=QuadGKJL(); abstol=nothing)
-    f = extrapolate(interpolate((x,), y, Gridded(Linear())), 0.0)
-    isnothing(abstol) && (abstol=1.0e-8*unit(eltype(x))*unit(eltype(y)))
-    sol = solve(IntegralProblem((x, p)->f(x), a, b), method; abstol)
-    sol.retcode ≠ ReturnCode.Success && error("solution unsuccessful with code: $(sol.retcode)")
-    return sol.u
-end
-
-function integrate(y::AbstractMatrix, x::AbstractVector, a::Number, b::Number, method::AbstractIntegralAlgorithm=QuadGKJL(); abstol=nothing)
-    isnothing(abstol) && (abstol=1.0e-8*unit(eltype(x))*unit(eltype(y)))
-    sol = ones(eltype(x[1]*y[1,1]), size(y)[1])
-    for (i, yᵢ) in enumerate(eachrow(y))
-        fᵢ = extrapolate(interpolate((x,), yᵢ, Gridded(Linear())), 0.0)
-        solᵢ = solve(IntegralProblem((x, p)->fᵢ(x), a, b), method; abstol)
-        solᵢ.retcode ≠ ReturnCode.Success && error("solution $i unsuccessful with code: $(sol.retcode)")
-        sol[i] = solᵢ.u
-    end
-    return sol
-end
-
-function integrate(y::AbstractMatrix, x::AbstractVector, a::Union{Number, AbstractVector}, b::Union{Number, AbstractVector}, method::AbstractIntegralAlgorithm=QuadGKJL(); abstol=nothing)
-    isnothing(abstol) && (abstol=1.0e-8*unit(eltype(x))*unit(eltype(y)))
-    sol = ones(eltype(x[1]*y[1,1]), size(y)[1])
-    a = (length(a)≠1) ? a : ones(size(y)[1])*a
-    b = (length(b)≠1) ? b : ones(size(y)[1])*b
-    for (i, yᵢ) in enumerate(eachrow(y))
-        fᵢ = extrapolate(interpolate((x,), yᵢ, Gridded(Linear())), 0.0)
-        solᵢ = solve(IntegralProblem((x, p)->fᵢ(x), a[i], b[i]), method; abstol)
-        solᵢ.retcode ≠ ReturnCode.Success && error("solution $i unsuccessful with code: $(sol.retcode)")
-        sol[i] = solᵢ.u
-    end
-    return sol
+function integrate(S::WaveSpectrum, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing; alg::AbstractIntegralAlgorithm=QuadGKJL())
+    isnothing(f_begin) && (f_begin = isunitful(S) ? 0Hz : 0)
+    isnothing(f_end) && (f_end = isunitful(S) ? Inf*Hz : Inf)
+    return integrate(S.func, f_begin, f_end; alg=alg)
 end
 
 ∫ = integrate
 
-function spectral_moment(S::AbstractVector, f::AbstractVector, n::Integer; method::AbstractIntegralAlgorithm=TrapezoidalRule())
-    return ∫((S.*(f.^n)), f, method)
+function spectral_moment(S::WaveSpectrum, n::Int, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing; alg::AbstractIntegralAlgorithm=QuadGKJL())
+    return ∫(f -> S(f)*f^n, f_begin, f_end; alg=alg)
 end
 
-function spectral_moment(S::AbstractMatrix, f::AbstractVector, n::Integer; method::AbstractIntegralAlgorithm=TrapezoidalRule())
-    return ∫((S.*(f.^n)'), f, method)
+function energy_period(S::WaveSpectrum, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing; alg::AbstractIntegralAlgorithm=QuadGKJL())
+    m_n1 = spectral_moment(S, -1, f_begin, f_end; alg=alg)
+    m_0 = spectral_moment(S, 0, f_begin, f_end; alg=alg)
+    return upreferred(m_n1/m_0)
 end
 
-function spectral_moment(S::AbstractVector, f::AbstractVector, n::Integer, a::Number, b::Number; method::AbstractIntegralAlgorithm=QuadGKJL())
-    return ∫((S.*(f.^n)), f, a, b, method)
-end
-
-function spectral_moment(S::AbstractMatrix, f::AbstractVector, n::Integer, a::Union{Number, AbstractVector}, b::Union{Number, AbstractVector}; method::AbstractIntegralAlgorithm=QuadGKJL())
-    return ∫((S.*(f.^n)'), f, a, b, method)
-end
-
-function energy_period(S::AbstractVecOrMat, f::AbstractVector; method::AbstractIntegralAlgorithm=TrapezoidalRule())
-    m_n1 = spectral_moment(S, f, -1; method)
-    m_0 = spectral_moment(S, f, 0; method)
-    return upreferred.(m_n1./m_0)
-end
-
-function energy_period(S::WaveSpectrum; method::AbstractIntegralAlgorithm=TrapezoidalRule())
-    m_n1 = spectral_moment(S.density, S.frequency, -1; method)
-    m_0 = spectral_moment(S.density, S.frequency, 0; method)
-    return upreferred.(m_n1./m_0)
-end
-
-function significant_waveheight(S::AbstractVecOrMat, f::AbstractVector; method::AbstractIntegralAlgorithm=TrapezoidalRule())
-	m_0 = spectral_moment(S, f, 0; method)
-    return 4(.√m_0)
-end
-
-function significant_waveheight(S::WaveSpectrum; method::AbstractIntegralAlgorithm=TrapezoidalRule())
-    m_0 = spectral_moment(S.density, S.frequency, 0; method)
-    return 4(.√m_0)
+function significant_waveheight(S::WaveSpectrum, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing; alg::AbstractIntegralAlgorithm=QuadGKJL())
+	m_0 = spectral_moment(S, 0, f_begin, f_end; alg=alg)
+    return 4√m_0
 end
