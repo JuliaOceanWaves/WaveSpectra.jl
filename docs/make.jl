cf6d9d4f0c1e968b8e6b94d69456f1e12163cba5
Rafael Baez Ramirez <rbaezra@sandia.gov>
2024-06-04 13:23:19 -0600

    Julia Debug

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Documenter, Example
 
-JULIA_DEBUG=Documenter
+ENV["JULIA_DEBUG"]=Documenter
 makedocs(sitename="WaveSpectra")
 
 deploydocs(repo="github.com/JuliaOceanWaves/WaveSpectra.jl.git",)
