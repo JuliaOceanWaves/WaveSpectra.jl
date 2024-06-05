9943b19c40a683b4afb71c8635523e7c38c3764d
Carlos A. Michelén Ströfer <c.michelen.strofer@gmail.com>
2024-06-05 12:27:31 -0600

    re-organizing

--- src/WaveSpectra.jl
+++ src/WaveSpectra.jl
@@ -1,138 +1,18 @@
 module WaveSpectra
 
-export WaveSpectrum, isdiscrete, isunitful, quantity
-# export integrate, ∫, spectral_moment, energy_period, significant_waveheight
+export OmnidirectionalSpectrum, isdensity, isdiscrete, isunitful, quantity
+export integrate, spectral_moment, energy_period, significant_waveheight
 
+# using Base: Base # extend: showerror
+using Unitful: Unitful # extend: unit, dimension
 using Interpolations: linear_interpolation
 using Plots: @recipe
-using Unitful: Frequency, Hz, upreferred
-import Unitful: unit, dimension, Quantity
-using Integrals: IntegralProblem, solve, QuadGKJL
+using Unitful: Dimensions, Frequency, NoDims, Quantity, Hz, dimension, unit, upreferred, 𝐓, 𝐋
+using Integrals: IntegralProblem, QuadGKJL, solve
 using SciMLBase: AbstractIntegralAlgorithm, ReturnCode
-import Base: Exception, showerror
 
+include("omnidirectional_spectra.jl")
+# include("spreading_functions.jl")
+# include("direction_spectra.jl")
 
-# Struct definition
-struct WaveSpectrum{D, U}
-    func::Union{Nothing, Function}
-    frequency::Union{Nothing, AbstractVector{<:Number}}
-    value::Union{Nothing, AbstractVector{<:Number}}
-
-    function WaveSpectrum{D,U}(func, frequency, value) where {D,U}
-        # check parameters
-        if !(isa(D, Bool) && isa(U, Bool))
-            error("parameters must be booleans")
-        end
-        # check arguments
-        option_1 = !isnothing(func) && isnothing(frequency) && isnothing(value) # T F F
-        option_2 = !isnothing(func) && !isnothing(frequency) && isnothing(value) # T T F
-        option_3 = isnothing(func) && !isnothing(frequency) && !isnothing(value) # F T T
-        function check_frequency(frequency)
-            (eltype(frequency)<:Frequency) || error("unknown units for `frequency` vector")
-        end
-        if !(option_1 || option_2 || option_3)
-            error("bad combination of input arguments")
-        end
-        if option_3 && (size(frequency) !== size(value))
-            error("`frequency` and `value` arrays are not same size!")
-        end
-        (option_2||option_3) && U && check_frequency(frequency)
-        # create parametric object
-        option_2 && (value = func.(frequency))
-        return new{D,U}(func, frequency, value)
-    end
 end
-
-# Constructors
-function WaveSpectrum(func::Function; density=true, units=true)
-    # option 1
-    return WaveSpectrum{density}{units}(func, nothing, nothing)
-end
-
-function WaveSpectrum(func::Function, freqs::AbstractVector{<:Number}; density=true)
-    # option 2
-    units = (eltype(freqs) <: Quantity) ? true : false
-    return WaveSpectrum{density}{units}(func, freqs)
-end
-
-function WaveSpectrum(S::WaveSpectrum, freqs::AbstractVector{<:Number})
-    # option 2b
-    density, units = typeof(S).parameters
-    return WaveSpectrum(S.func, freqs; density, units)
-end
-
-function WaveSpectrum(freqs::AbstractVector{<:Number}, vals::AbstractVector{<:Number}; density=true, interpolation::Function=linear_interpolation)
-    # option 3
-    units = (eltype(vals) <: Quantity) ? true : false
-    func = x->interpolation(freqs, vals; extrapolation_bc = 0)(x)
-    S = WaveSpectrum{density, units}(func, freqs, nothing)
-    @assert (S.value  ≈ vals)
-    return S
-end
-
-# Interface
-isdiscrete(S::WaveSpectrum) = !isnothing(S.value) && !isnothing(S.frequency)
-isdensity(S::WaveSpectrum) = typeof(S) <: WaveSpectrum{true, U} where U
-isunitful(S::WaveSpectrum) = typeof(S) <: WaveSpectrum{D, true} where D
-unit(S::WaveSpectrum) = unit(eltype(S.value))
-dimension(S::WaveSpectrum) = dimension(eltype(S.value))
-function quantity(S::WaveSpectrum{true,true})
-    dims = dimension(S) * dimension(eltype(S.frequency))
-    units = upreferred(unit(S) * unit(eltype(S.frequency)))
-    return dims, units
-end
-
-(S::WaveSpectrum)(freq::Number) =  S.func(freq)
-
-mutable struct DiscreteError <: Exception
-    message::String
-    DiscreteError() = new("continuous `WaveSpectrum` instance was treated as discrete")
-end
-Base.showerror(io::IO, e::DiscreteError) = print(io, "DiscreteError: $(e.message)")
-
-_firststate() = 1
-Base.getindex(S::WaveSpectrum, i::Int) = isdiscrete(S) ? S.value[i] : throw(DiscreteError())
-Base.firstindex(S::WaveSpectrum) = isdiscrete(S) ? _firststate() : throw(DiscreteError())
-Base.lastindex(S::WaveSpectrum) = isdiscrete(S) ? length(S.value) : throw(DiscreteError())
-Base.length(S::WaveSpectrum) = isdiscrete(S) ? length(S.value) : throw(DiscreteError())
-Base.size(S::WaveSpectrum) = isdiscrete(S) ? size(S.value) : throw(DiscreteError())
-Base.eltype(S::WaveSpectrum) = isdiscrete(S) ? eltype(S.value) : throw(DiscreteError())
-Base.iterate(S::WaveSpectrum) = isdiscrete(S) ? (S.value[firstindex(S)], _firststate()+1) : throw(DiscreteError())
-function Base.iterate(S::WaveSpectrum, state::Int)
-    isdiscrete(S) || throw(DiscreteError())
-    return (state<=length(S)) ? (S.value[state], state+1) : nothing
-end
-
-# Plots recipes
-function _labels(S::WaveSpectrum)
-    x_label = "frequency"
-    y_label = isdensity(S) ? "spectral density" : "discrete (integral) spectrum"
-    return (x_label, y_label)
-end
-
-@recipe function f(S::WaveSpectrum, args...)
-    _xlabel, _ylabel = _labels(S)
-    xlabel --> _xlabel
-    ylabel --> _ylabel
-    if isdiscrete(S)
-        marker := :auto
-        (S.frequency, S.value, args...)
-    else
-        if isempty(args)
-            isunitful(S) ? (args=(0Hz, 10Hz)) : (args=(0, 10))
-        end
-        (S.func, args...)
-    end
-end
-
-@recipe function f(S::WaveSpectrum, freq::AbstractVector{<:Number}, args...)
-    _xlabel, _ylabel = _labels(S)
-    xlabel --> _xlabel
-    ylabel --> _ylabel
-    marker := :auto
-    (freq, S.(freq), args...)
-end
-
-# include("statistics.jl")
-
-end #Module end
---
+++ src/omnidirectional_spectra.jl
@@ -0,0 +1,190 @@
+# Struct definition
+struct OmnidirectionalSpectrum{S, F, D}
+    func::Function
+    discrete::Union{Nothing, @NamedTuple begin
+        frequency::AbstractVector
+        value::AbstractVector
+    end}
+
+    function OmnidirectionalSpectrum{S, F, D}(func, discrete) where {S<:Dimensions, F<:Dimensions, D}
+        # check parameters
+        !isa(D, Bool) && error("parameter `D` must be a boolean")
+        if !(isa(1*upreferred(F()), Frequency) || (F == typeof(NoDims)))
+            error("parameter `F` must be a `Unitful.Frequency` or `Unitful.NoDims`")
+        end
+        if (F == typeof(NoDims)) && !(S == typeof(NoDims))
+            error("if `F` is dimensionless `S` must be too")
+        end
+        # check arguments
+        if !(isnothing(discrete))
+            if (length(discrete.frequency) !== length(discrete.value))
+                error("`frequency` and `value` arrays must have same size")
+            end
+            if func.(discrete.frequency) ≉ discrete.value
+                error("provided function and values do not agree")
+            end
+            ftype = eltype(discrete.frequency)
+            stype = eltype(discrete.value)
+            if !(ftype<:Frequency || dimension(ftype)==NoDims)
+                error("`discrete.frequency` must have a frequency dimension or no dimension")
+            end
+            if dimension(ftype)==NoDims && !(dimension(stype)==NoDims)
+                error("if `discrete.frequency` is dimensionless, `discrete.value` must be too")
+            end
+        end
+        # create parametric object
+        return new{S, F, D}(func, discrete)
+    end
+end
+
+# Constructors
+function OmnidirectionalSpectrum(func::Function; dims_frequency::Dimensions=𝐓^-1, dims_value::Dimensions=𝐋^2*𝐓, density=true)
+    return OmnidirectionalSpectrum{typeof(dims_value), typeof(dims_frequency), density}(func, nothing)
+end
+
+function OmnidirectionalSpectrum(func::Function, freqs::AbstractVector{<:Number}; density=true)
+    dims_frequency = dimension(eltype(freqs))
+    dims_value = dimension(func(0*unit(eltype(freqs))))
+    discrete = (
+        frequency = freqs,
+        value = func.(frequency)
+    )
+    return OmnidirectionalSpectrum{typeof(dims_value), typeof(dims_frequency), density}(func, discrete)
+end
+
+function OmnidirectionalSpectrum(spectrum::OmnidirectionalSpectrum, freqs::AbstractVector{<:Number})
+    Ts, Tf, density = typeof(spectrum).parameters
+    discrete = (
+            frequency = freqs,
+            value = spectrum.func.(freqs)
+        )
+    return OmnidirectionalSpectrum{Ts, Tf, density}(spectrum.func, discrete)
+end
+
+function OmnidirectionalSpectrum(freqs::AbstractVector{<:Number}, vals::AbstractVector{<:Number}; density=true, interpolation::Function=linear_interpolation)
+    dims_frequency = dimension(eltype(freqs))
+    dims_value = dimension(eltype(vals))
+    discrete = (
+        frequency = freqs,
+        value = vals
+    )
+    func = x->interpolation(freqs, vals; extrapolation_bc = 0*unit(eltype(vals)))(x)
+    @assert func.(freqs) ≈ vals
+    return OmnidirectionalSpectrum{typeof(dims_value), typeof(dims_frequency), density}(func, discrete)
+end
+
+# Interface
+isdiscrete(spectrum::OmnidirectionalSpectrum) = !isnothing(spectrum.discrete)
+isdensity(spectrum::OmnidirectionalSpectrum) = typeof(spectrum) <: OmnidirectionalSpectrum{S, F, true} where {S,F}
+isunitful(spectrum::OmnidirectionalSpectrum{Ts, Tf, D})  where {Ts, Tf, D} = isa(1*upreferred(Tf()), Frequency)
+Unitful.unit(spectrum::OmnidirectionalSpectrum{Ts, Tf, D})  where {Ts, Tf, D} = isdiscrete(spectrum) ? unit(eltype(speectrum.discrete.value)) : unit(spectrum(0*upreferred(Tf())))
+Unitful.dimension(spectrum::OmnidirectionalSpectrum{Ts, Tf, D}) where {Ts, Tf, D} = Ts()
+function quantity(spectrum::OmnidirectionalSpectrum{Ts, Tf, D}) where {Ts, Tf, D}
+    dims = Ts()*Tf()
+    units = upreferred(dims)
+    return dims, units
+end
+
+(spectrum::OmnidirectionalSpectrum)(freq::Number) =  spectrum.func(freq)
+
+mutable struct DiscreteError <: Exception
+    message::String
+    DiscreteError() = new("continuous `OmnidirectionalSpectrum` instance was treated as discrete")
+end
+Base.showerror(io::IO, e::DiscreteError) = print(io, "DiscreteError: $(e.message)")
+
+_firststate() = 1
+Base.getindex(spectrum::OmnidirectionalSpectrum, i::Int) = isdiscrete(spectrum) ? spectrum.discrete.value[i] : throw(DiscreteError())
+Base.getindex(spectrum::OmnidirectionalSpectrum, I) = isdiscrete(spectrum) ? [spectrum.discrete.value[i] for i in I] : throw(DiscreteError())
+Base.firstindex(spectrum::OmnidirectionalSpectrum) = isdiscrete(spectrum) ? _firststate() : throw(DiscreteError())
+Base.lastindex(spectrum::OmnidirectionalSpectrum) = isdiscrete(spectrum) ? length(spectrum.discrete.value) : throw(DiscreteError())
+Base.length(spectrum::OmnidirectionalSpectrum) = isdiscrete(spectrum) ? length(spectrum.discrete.value) : throw(DiscreteError())
+Base.size(spectrum::OmnidirectionalSpectrum) = isdiscrete(spectrum) ? size(spectrum.discrete.value) : throw(DiscreteError())
+Base.eltype(spectrum::OmnidirectionalSpectrum) = isdiscrete(spectrum) ? eltype(spectrum.discrete.value) : throw(DiscreteError())
+Base.iterate(spectrum::OmnidirectionalSpectrum) = isdiscrete(spectrum) ? (spectrum.discrete.value[firstindex(spectrum)], _firststate()+1) : throw(DiscreteError())
+function Base.iterate(spectrum::OmnidirectionalSpectrum, state::Int)
+    isdiscrete(spectrum) || throw(DiscreteError())
+    return (state<=length(spectrum)) ? (spectrum.discrete.value[state], state+1) : nothing
+end
+
+# Plots recipes
+function _labels(spectrum::OmnidirectionalSpectrum)
+    x_label = "frequency"
+    y_label = isdensity(spectrum) ? "spectral density" : "discrete (integral) spectrum"
+    return (x_label, y_label)
+end
+
+@recipe function f(spectrum::OmnidirectionalSpectrum{Ts, Tf, D}, args...) where {Ts, Tf, D}
+    _xlabel, _ylabel = _labels(spectrum)
+    xlabel --> _xlabel
+    ylabel --> _ylabel
+    if isdiscrete(spectrum)
+        marker := :auto
+        (spectrum.discrete.frequency, spectrum.discrete.value, args...)
+    else
+        isempty(args) && (args=(0*upreferred(Tf()), 10*upreferred(Tf())))
+        (spectrum.func, args...)
+    end
+end
+
+@recipe function f(spectrum::OmnidirectionalSpectrum, freq::AbstractVector{<:Number}, args...)
+    _xlabel, _ylabel = _labels(spectrum)
+    xlabel --> _xlabel
+    ylabel --> _ylabel
+    marker := :auto
+    (freq, spectrum.(freq), args...)
+end
+
+# integral/statistics
+function _integrate(func::Function, f_begin::Union{Number, Nothing}=nothing,
+        f_end::Union{Number, Nothing}=nothing, alg::AbstractIntegralAlgorithm=QuadGKJL();
+        kwargs...
+    )
+    sol = solve(IntegralProblem((x,p)->func(x), (f_begin, f_end), nothing), alg; kwargs...)
+    sol.retcode ≠ ReturnCode.Success && error("solution unsuccessful with code: $(sol.retcode)")
+    return upreferred(sol.u)
+end
+
+function _update_domain(spectrum::OmnidirectionalSpectrum{Ts, Tf, D}, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing) where {Ts, Tf, D}
+    isnothing(f_begin) && (f_begin = 0*upreferred(Tf()))
+    isnothing(f_end) && (f_end = 10*upreferred(Tf()))
+    return (f_begin, f_end)
+end
+
+function integrate(
+    spectrum::OmnidirectionalSpectrum{Ts, Tf, D},
+        f_begin::Union{Number, Nothing}=nothing,
+        f_end::Union{Number, Nothing}=nothing;
+        alg::AbstractIntegralAlgorithm=QuadGKJL(),
+        kwargs...
+    ) where {Ts, Tf, D}
+    f_begin, f_end = _update_domain(spectrum, f_begin, f_end)
+    if :abstol ∉ keys(kwargs)
+        abstol = 1e-8*upreferred(Ts()*Tf())
+        kwargs = merge(values(kwargs), (abstol=abstol,))
+    end
+    return _integrate(spectrum.func, f_begin, f_end, alg; kwargs...)
+end
+
+function spectral_moment(spectrum::OmnidirectionalSpectrum{Ts, Tf, D}, n::Int, f_begin::Union{Number, Nothing}=nothing,
+        f_end::Union{Number, Nothing}=nothing, alg::AbstractIntegralAlgorithm=QuadGKJL();
+        kwargs...
+    ) where {Ts, Tf, D}
+    f_begin, f_end = _update_domain(spectrum, f_begin, f_end)
+    if :abstol ∉ keys(kwargs)
+        abstol = 1e-8*upreferred(Ts()*Tf()^(n+1))
+        kwargs = merge(values(kwargs), (abstol=abstol,))
+    end
+    return _integrate(f -> spectrum(f)*f^n, f_begin, f_end, alg; kwargs...)
+end
+
+function energy_period(spectrum::OmnidirectionalSpectrum, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing, alg::AbstractIntegralAlgorithm=QuadGKJL(); kwargs...)
+    m_n1 = spectral_moment(spectrum, -1, f_begin, f_end, alg; kwargs...)
+    m_0 = spectral_moment(spectrum, 0, f_begin, f_end, alg; kwargs...)
+    return m_n1/m_0
+end
+
+function significant_waveheight(spectrum::OmnidirectionalSpectrum, f_begin::Union{Number, Nothing}=nothing, f_end::Union{Number, Nothing}=nothing, alg::AbstractIntegralAlgorithm=QuadGKJL(); kwargs...)
+	m_0 = spectral_moment(spectrum, 0, f_begin, f_end, alg; kwargs...)
+    return 4√m_0
+end
