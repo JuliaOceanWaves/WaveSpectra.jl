a3302ffc2e30dd9b91fc022aeb81b94cb2740832
Rafael Baez Ramirez <rbaezra@sandia.gov>
2024-06-10 16:26:26 -0600

    Included discrete spectrum struct

--- src/omnidirectional_spectra.jl
+++ src/omnidirectional_spectra.jl
@@ -1,5 +1,5 @@
 
 
 include("omnidirectional_spectra/continuous.jl")
-# include("omnidirectional_spectra/discrete.jl")
+include("omnidirectional_spectra/discrete.jl")
 include("omnidirectional_spectra/ocean_waves.jl")
