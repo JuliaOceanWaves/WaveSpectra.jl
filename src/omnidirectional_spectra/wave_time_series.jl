978c73ece074218b5e51a4f05bd0b7865915e56d
Rafael Baez Ramirez <rbaezra@sandia.gov>
2024-07-08 09:00:05 -0600

    Minor Change before adding NFFT

--- src/omnidirectional_spectra/wave_time_series.jl
+++ src/omnidirectional_spectra/wave_time_series.jl
@@ -1,22 +1,22 @@
 struct WaveTimeSeries{TS<:Quantity, TF<:Quantity, N} <: AbstractArray{TS, N}
-    value::AbstractVecOrMat{<:Quantity}
     time::AbstractVector{<:Quantity}
+    values::AbstractVecOrMat{<:Quantity}
     colnames::Union{AbstractVector{<:AbstractString}, Nothing}
     meta::Any
-    function WaveTimeSeries(value::AbstractVecOrMat{<:Quantity}, time::AbstractVector{<:Quantity}, colnames=nothing, meta=nothing)
+    function WaveTimeSeries(values::AbstractVecOrMat{<:Quantity}, time::AbstractVector{<:Quantity}, colnames=nothing, meta=nothing)
         # Parameters
-        N=ndims(value) # Don't need to check N if AbstractVecOrMat handles 0 dim and 3+ dims
+        N=ndims(values) # Don't need to check N if AbstractVecOrMat handles 0 dim and 3+ dims
         TF=eltype(time)
-        TS=eltype(value)
+        TS=eltype(values)
         # Check Arguments
-        (size(value)[1] ≠ length(time)) && error("'time' and first dimension of 'value' array must be same size")
-        return new{TS,TF,N}(value, time)
+        (size(values)[1] ≠ length(time)) && error("'time' and first dimension of 'values' array must be same size")
+        return new{TS,TF,N}(values, time)
     end
 end
 
 #Constructors
-function WaveTimeSeries(spectrum::DiscreteOmnidirectionalSpectrum{TS, TF, D, 1}; seed=nothing) where {TS, TF, D}
-    values, freqs = spectrum.value, spectrum.frequency
+function WaveTimeSeries(spectrum::DiscreteOmnidirectionalSpectrum{TS, TF, D, 1}; r_amps=nothing, seed=nothing) where {TS, TF, D}
+    values, freqs = spectrum.values, spectrum.frequency
     # @assert typeof(freqs) <: AbstractRange "Frequencies must be a range, unevenly spaces frequencies are not yet implemented!"
     n_samples = size(values, 1)
     tend = isapprox(0.0, freqs[begin]) ? 2π/freqs[2] : 2π/freqs[begin]
@@ -33,15 +33,15 @@ function WaveTimeSeries(spectrum::DiscreteOmnidirectionalSpectrum{TS, TF, D, 1};
 end
 
 #Interface
-Base.size(series::WaveTimeSeries) = size(series.value)
-Base.getindex(series::WaveTimeSeries, i::Int) = series.value[i]
-Base.getindex(series::WaveTimeSeries, I::Vararg{Int, 2}) = series.value[I...]
-Base.getindex(series::WaveTimeSeries, I) = [series.value[i] for i in I]
-Base.length(series::WaveTimeSeries) = length(series.value)
-Base.iterate(series::WaveTimeSeries) = isempty(series.value) ? nothing : (series.value[_firstState()], _firstState() + 1)
-Base.iterate(series::WaveTimeSeries, state::Int) = (state <= length(series.value)) ? (series.value[state], state+1) : nothing
+Base.size(series::WaveTimeSeries) = size(series.values)
+Base.getindex(series::WaveTimeSeries, i::Int) = series.values[i]
+Base.getindex(series::WaveTimeSeries, I::Vararg{Int, 2}) = series.values[I...]
+Base.getindex(series::WaveTimeSeries, I) = [series.values[i] for i in I]
+Base.length(series::WaveTimeSeries) = length(series.values)
+Base.iterate(series::WaveTimeSeries) = isempty(series.values) ? nothing : (series.values[_firstState()], _firstState() + 1)
+Base.iterate(series::WaveTimeSeries, state::Int) = (state <= length(series.values)) ? (series.values[state], state+1) : nothing
 Base.firstindex(::WaveTimeSeries) = _firstState()
-Base.lastindex(series::WaveTimeSeries) = length(series.value)
+Base.lastindex(series::WaveTimeSeries) = length(series.values)
 Base.eltype(::WaveTimeSeries{TS}) where {TS} = TS
 
 
@@ -58,11 +58,11 @@ function Base.show(io::IO, ::MIME"text/plain", series::WaveTimeSeries{TS,TF,N}) 
     end
 
     if N < 2
-        av = round.(ustrip.(series.value), digits=3)
+        av = round.(ustrip.(series.values), digits=3)
         av = length(av) > 2 ? join([av[begin], av[end]], " … ") : av
     else
         lrow_str = "\n\t\t\t⋮   ⋱   ⋮\n\t\t\t"
-        erow = [round.(r,digits=3) for r in eachrow(ustrip.(series.value))]
+        erow = [round.(r,digits=3) for r in eachrow(ustrip.(series.values))]
         rows = [length(r) > 2 ? join([r[begin], r[end]], " … ") : join(r, ", ") for r in erow]
         av = length(rows) > 2 ? join([rows[begin], rows[end]], lrow_str) : join(rows, ", \n")
     end
@@ -73,5 +73,5 @@ end
     xlabel --> "Time"
     ylabel --> "Wave Elevation"
     marker := :auto
-    (series.time, series.value, args...)
+    (series.time, series.values, args...)
 end
