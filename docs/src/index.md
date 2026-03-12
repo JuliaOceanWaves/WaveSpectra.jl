a55d76aa8f180a5c543f38058019606e72458c49
Carlos A. Michelén Ströfer <c.michelen.strofer@gmail.com>
2026-03-12 14:09:47 -0600

    Setup Docs and Tests (placeholders)

--- docs/src/index.md
+++ docs/src/index.md
@@ -3,12 +3,13 @@
 ## Overview
 
 WaveSpectra.jl provides a unit-aware tool for working with ocean wave spectra.
-This package enables input, conversion, creation, and characterization 
+This package enables input, conversion, creation, and characterization
 for both polar, cartesian, and omnidirectional wave spectra.
 
 ## Quick start
 
 ```julia
+using DimensionfulAngles.DefaultSymbols
 using WaveSpectra
 
 Nf = 20
@@ -21,23 +22,6 @@ Nθ = 36
 S = Spectrum(randn(Nf, Nθ)*m^2/Hz/°, f, θ)
 ```
 
-## Detailed usage
-
-### Creating and inputting spectra
-
-
-
-### Spectrum conversion
-
-
-
-### Spectrum characterization
-
-
-
 ## Reference
 
 See the [API reference](@ref API) for all exported functions.
-
-## Theory
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
-using Test
+using Test, SafeTestsets, Documenter
 
-@testset "test_core" begin
-    include("test_core.jl")
+@time @testset verbose=true "WaveSpectra.jl" begin
+    @time @safetestset "Test Core Capabilities" begin
+        include("test_core.jl")
+    end
+    @time @safetestset "Test Split Spectrum" begin
+        include("test_splitspectrum.jl")
+    end
+    @time @safetestset "Test Integration" begin
+        include("test_integration.jl")
+    end
+    @time @safetestset "Test Conversion" begin
+        include("test_conversion.jl")
+    end
+    @time @safetestset "Test Dispersion Relations" begin
+        include("test_dispersion.jl")
+    end
+    @time @safetestset "Test Parametric Spectra" begin
+        include("test_parametric.jl")
+    end
 end
