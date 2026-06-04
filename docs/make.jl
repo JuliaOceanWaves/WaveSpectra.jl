fc6ea757fa51be1cd168a9721a3e13697df7a769
Carlos A. Michelén Ströfer <c.michelen.strofer@gmail.com>
2026-06-04 09:55:28 -0600

    Update docs/make.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/make.jl
+++ docs/make.jl
@@ -13,22 +13,22 @@ function regex_match(regex, x)
 end
 
 makedocs(
-	sitename = "WaveSpectra.jl",
-	format = Documenter.HTML(
-		assets = String["assets/citations.css",
-			"assets/images.css"],
-	),
-	modules = [WaveSpectra],
-	pages = [
-		"Home" => "index.md",
-		"Package Guide" => ["Quickstart" => "guide/quickstart.md",
-			"Spectra" => "guide/spectra.md",
-			"Working with Spectra" => "guide/spectra_funcs.md",
-			"Other Functions" => "guide/other.md"],
-		"API" => "api.md",
-	],
-	plugins = [bib],
-	warnonly = true,
+    sitename = "WaveSpectra.jl",
+    format = Documenter.HTML(
+        assets = String["assets/citations.css",
+        "assets/images.css"],
+    ),
+    modules = [WaveSpectra],
+    pages = [
+        "Home" => "index.md",
+        "Package Guide" => ["Quickstart" => "guide/quickstart.md",
+            "Spectra" => "guide/spectra.md",
+            "Working with Spectra" => "guide/spectra_funcs.md",
+            "Other Functions" => "guide/other.md"],
+        "API" => "api.md"
+    ],
+    plugins = [bib],
+    warnonly = true
 )
 
 deploydocs(; repo = "github.com/JuliaOceanWaves/WaveSpectra.jl.git",
