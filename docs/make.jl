e962aaf54d73a79005bf3400c52e75d917f27105
Rafael Baez Ramirez <rbaezra@sandia.gov>
2026-06-08 09:33:22 -0600

    Updated configuration files to match RepoTemplate.jl

--- docs/make.jl
+++ docs/make.jl
@@ -9,28 +9,31 @@ ENV["UNITFUL_FANCY_EXPONENTS"] = true
 bib = CitationBibliography(joinpath(@__DIR__, "references.bib"))
 
 function regex_match(regex, x)
-    return !isnothing(match(regex, string(x)))
+	return !isnothing(match(regex, string(x)))
 end
 
 makedocs(
-    sitename = "WaveSpectra.jl",
-    format = Documenter.HTML(
-        assets = String["assets/citations.css",
-        "assets/images.css"],
-    ),
-    modules = [WaveSpectra],
-    pages = [
-        "Home" => "index.md",
-        "Package Guide" => ["Quickstart" => "guide/quickstart.md",
-            "Spectra" => "guide/spectra.md",
-            "Working with Spectra" => "guide/spectra_funcs.md",
-            "Other Functions" => "guide/other.md"],
-        "API" => "api.md"
-    ],
-    plugins = [bib],
-    warnonly = true
+	sitename = "WaveSpectra.jl",
+	format = Documenter.HTML(
+		prettyurls = get(ENV, "CI", "false") == "true",
+		assets = String["assets/citations.css",
+			"assets/images.css"],
+	),
+	modules = [WaveSpectra],
+	pages = [
+		"Home" => "index.md",
+		"Package Guide" => ["Quickstart" => "guide/quickstart.md",
+			"Spectra" => "guide/spectra.md",
+			"Working with Spectra" => "guide/spectra_funcs.md",
+			"Other Functions" => "guide/other.md"],
+		"API" => "api.md",
+	],
+	plugins = [bib],
 )
 
-deploydocs(; repo = "github.com/JuliaOceanWaves/WaveSpectra.jl.git",
-    push_preview = true,
-    versions = ["latest" => "v^", "v#"])
+deploydocs(
+	repo = "github.com/JuliaOceanWaves/WaveSpectra.jl.git",
+	devbranch = "main",
+	push_preview = true,
+	versions = ["latest" => "v^", "v#.#"],
+)
